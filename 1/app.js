6daac08e572884270df06b4732f3f2256304e5aa
David Jm <danimax.com@gmail.com>
2021-04-17 18:22:16 -0500

    Added async

--- 1/app.js
+++ 1/app.js
@@ -3,4 +3,7 @@ const { createFile } = require("./helpers/multiplicar");
 console.clear();
 
 const base = 5;
-createFile(base);
+
+createFile(base)
+    .then(filename => console.log(filename, "Created"))
+    .catch(err => console.error(err));
--- 1/helpers/multiplicar.js
+++ 1/helpers/multiplicar.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
 
-const createFile = (base) => {
+const createFile = async (base) => {
 
-    let output = '';
+    try {
+        let output = '';
+        const fileName = `tabla-${base}.txt`;
 
-    for (let i = 1; i <= 10; i++) {
-        output += `${base} X ${i} = ${(base * i)}\n`
+        for (let i = 1; i <= 10; i++) {
+            output += `${base} X ${i} = ${(base * i)}\n`
+        }
+
+        fs.writeFile(fileName, output, (error) => {
+            if (error) throw error
+
+            console.log('Archivo creado')
+        });
+
+        return fileName;
+
+    } catch (error) {
+        throw error;
     }
-
-    fs.writeFile(`tabla-${base}.txt`, output, (error) => {
-        if (error) throw error
-
-        console.log('Archivo creado')
-    });
 }
 
 module.exports = {
