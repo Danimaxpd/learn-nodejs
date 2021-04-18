3186fe3aa6ea7ca057b5471a51ef0dfbf20cd687
David Jm <danimax.com@gmail.com>
2021-04-18 12:32:36 -0500

    playing yargssss

--- 1/app.js
+++ 1/app.js
@@ -1,9 +1,14 @@
 const { createFile } = require("./helpers/multiplicar");
-
+const argv = require('yargs').argv;
 console.clear();
 
-const base = 5;
+console.log(process.argv);
+console.log(argv);
 
-createFile(base)
-    .then(filename => console.log(filename, "Created"))
-    .catch(err => console.error(err));
+console.log("esta es la base", argv.base);
+
+// const base = 5;
+
+// createFile(base)
+//     .then(filename => console.log(filename, "Created"))
+//     .catch(err => console.error(err));
